caa9acc2cb476f874d139f6bc4ce58f0523bbd79
Gwilymm <marin.davies@gmail.com>
2024-02-19 09:28:33 +0100

    belafbalbdo

--- src/app/tools/gps/page.js
+++ src/app/tools/gps/page.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
 export default function GeolocationPage() {
 	const [ location, setLocation ] = useState({
