97713125fd92b0a92af1eab24c7c611164e1cc1f
Gwilymm <marin.davies@gmail.com>
2024-02-19 16:24:55 +0100

    Update .env and AuthButton.js files

--- src/components/AuthButton.js
+++ src/components/AuthButton.js
@@ -4,6 +4,7 @@ export default function AuthButton() {
 	const { data: session } = useSession()
 
 	if (session) {
+		console.log('session', session)
 		return (
 			<>
 				Signed in as {session.user.email} <br />
