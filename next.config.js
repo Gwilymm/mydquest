39c5f5bbf86100864da5515b847c1fc1ffc825dc
Gwilymm <marin.davies@gmail.com>
2024-02-19 15:53:04 +0100

    Add AuthButton component and update NEXTAUTH_URL

--- next.config.js
+++ next.config.js
@@ -1,22 +1,17 @@
-const withPWA = require("@ducanh2912/next-pwa").default;
+const withPWA = require("@ducanh2912/next-pwa").default({
+	dest: "public",
+	cacheOnFrontEndNav: true,
+	aggressiveFrontEndNavCaching: true,
+	reloadOnOnline: true,
+	swcMinify: true,
+	workboxOptions: {
+		disableDevLogs: true,
+	},
+	secret: 'your-long-randomly-generated-secret-string-here',
+});
 
 const nextConfig = {
 	// Vos autres options de configuration Next.js ici
 };
 
-module.exports = withPWA({
-	...nextConfig,
-	pwa: {
-		dest: "public",
-		cacheOnFrontEndNav: true,
-		aggressiveFrontEndNavCaching: true,
-		reloadOnOnline: true,
-		swcMinify: true,
-		workboxOptions: {
-			disableDevLogs: true,
-		},
-
-		// Autres options PWA que vous souhaitez configurer
-	},
-	secret: 'your-long-randomly-generated-secret-string-here',
-});
+module.exports = withPWA(nextConfig);
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { useSession, SessionProvider } from 'next-auth/react';
+import AuthButton from "../../components/AuthButton"
 
 export default function Example() {
 	return (
@@ -39,19 +40,7 @@ function LoginPage() {
 								{/* Add your email and password input fields here */}
 
 								<div>
-									{!session ? (
-										<button
-											onClick={() => signIn('google')}
-											className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
-										>
-											<svg className="h-5 w-5" aria-hidden="true" viewBox="0 0 24 24">
-												{/* Add Google icon SVG here */}
-											</svg>
-											<span className="text-sm font-semibold leading-6">Sign in with Google</span>
-										</button>
-									) : (
-										<button onClick={() => signOut()} className="font-semibold text-indigo-600 hover:text-indigo-500">Sign out</button>
-									)}
+									<AuthButton />
 								</div>
 							</form>
 						</div>
