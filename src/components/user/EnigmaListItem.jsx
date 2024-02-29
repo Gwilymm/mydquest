b34ca977acaf3064a410d0ac1b00837d589b0de6
Gwilymm <marin.davies@gmail.com>
2024-02-29 17:23:58 +0100

    Add handle function and refactor onClose event in EnigmaListItem.jsx

--- src/components/user/EnigmaListItem.jsx
+++ src/components/user/EnigmaListItem.jsx
@@ -5,6 +5,7 @@ import LockIcon from '@mui/icons-material/Lock'; // Icône pour les énigmes ver
 import EnigmaModal from '@/components/user/EnigmaModal'; // Assurez-vous que le chemin d'import est correct
 import axios from 'axios';
 import db from '@/utils/db'; // Vérifiez le chemin d'accès à votre base de données
+import handle from '@/pages/api/enigmas';
 
 // Fonction pour obtenir une URL d'image aléatoire de Unsplash
 const getRandomImageUrl = (seed, width, height) => `https://source.unsplash.com/${width}x${height}?sig=${seed}`;
@@ -22,6 +23,7 @@ const EnigmaListItem = ({ enigma, onDelete, isAccessible }) => {
       setTimeout(() => setIsShaking(false), 820); // Réinitialise l'état après l'animation
     }
   };
+  const handleCloseModal = () => setIsModalOpen(false);
 
 
   return (
@@ -66,7 +68,7 @@ const EnigmaListItem = ({ enigma, onDelete, isAccessible }) => {
       <EnigmaModal
         enigma={enigma}
         open={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
+        onClose={handleCloseModal}
        
       />
     </div>
